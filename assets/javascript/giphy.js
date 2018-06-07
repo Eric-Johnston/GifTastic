d4c55a676785bee62d05fbe4c5c16e9e558910f7
Eric J <ejohnston092@gmail.com>
2018-06-06 21:13:42 -0400

    New pseudo-code

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
     
     // Array of topics
     var topics = ["game of thrones", "star wars", "warhammer", "lord of the rings", "hiking", "snowboarding", "fractals", "the matrix", "elder scrolls", "fallout"];
-
     // This creates buttons for every string in the array.
     for (var j = 0; j < topics.length; j++){
         var newBtn = $("<button>").text(topics[j]).val(topics[j]);
@@ -16,14 +15,11 @@ $(document).ready(function(){
             var topic = $(this).val();
             var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
             topic + "&api_key=VAv72t9cFdirlQwvVfF5kxIMlFkrH0QS&rating=pg&limit=10";
-            console.log(topic);
             $.ajax({
                 url: queryURL,
                 method: "GET"
               })
               .then(function(response){
-                console.log(queryURL);
-                console.log(response);
                 var results = response.data
                 for (var i = 0; i < results.length; i++){
                     var p = $("<p>").text("Rating: " + results[i].rating);
@@ -35,24 +31,28 @@ $(document).ready(function(){
     }
     
     gifDisplay();
-    
     // Allows the user to add new topics to the array.
     $("#topic-submit").on("click", function(){
         event.preventDefault();
+        // If the input field is empty, display an alert
+        if ($("#topic-input").val().trim() == ""){
+            alert("Field can't be empty")
+        }
+        // Otherwise add new topic
+        else {
         var newTopic = $("#topic-input").val();
         var addBtn = $("<button>").text(newTopic).val(newTopic);
         $("#topic-buttons").append(addBtn);
+        // Pushes new topic into our array
         topics.push(newTopic);
         $("#topic-input").val("");
-        console.log(topics);
+        }
         
         gifDisplay();
     });
-
     // Initiates the pause/play function -- For some reasong using "img" or "<img>" as a selector wouldn't work, so I made it select any element in the body with a class of ".gif"
     $("body").on("click", ".gif", function() {
         var src = $(this).attr("src");
-        console.log(this)
         // Checks the image for the class "still"
       if($(this).hasClass("still")){
         // Replaces gif with images.fixed_height_still.url
